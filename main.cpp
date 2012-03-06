e4626c806ad08185f2e49697250636c89b743c1e
Marek Wawrzyczek <mwawrzyczek@gmail.com>
2012-03-06 22:59:57 +0100

    Created monitor.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 //
 #define N 2
 #define START_BALANCE 100
-#define MAX_PAYMENT 50
+#define MAX_PAYMENT 10
 #define MAX_PER_USER_ITERATIONS 1000000
 
 
@@ -21,23 +21,6 @@ class Bank
     static int enoutht_payment(int amount)
     {
         return Bank::balance >= amount ? 1 : 0;
-    }
-
-    static void add_money(int amount, int thread_id)
-    {
-        printf("thread %d enters add_money with amount=%d\n", thread_id, amount);
-        
-        pthread_mutex_lock(&mutex);
-        balance += amount;
-        pthread_mutex_unlock(&mutex);
-        
-        printf("balance: %d\n", balance);
-
-        printf("before thread %d signaling money added\n", thread_id);
-
-        pthread_cond_signal(&cond);
-
-        printf("after thread %d signaling money added\n", thread_id);
     }
 
     static void take_money(int amount, int thread_id)
@@ -57,36 +40,36 @@ class Bank
         printf("thread %d removed %d from balance. now balance is: %d\n", thread_id, amount, balance);
     }
 
-    public: static void* bank_user(void *args)
+    public: static void add_money(int amount, int thread_id)
     {
-        int i = MAX_PER_USER_ITERATIONS;
-        int amount;
-        int thread_number = *((int *)args);
+        printf("thread %d enters add_money with amount=%d\n", thread_id, amount);
+        
+        pthread_mutex_lock(&mutex);
+        balance += amount;
+        pthread_mutex_unlock(&mutex);
+        
+        printf("balance: %d\n", balance);
 
-        printf("here's thread %d\n", thread_number);
-        while(i--)
-        {
-            //pthread_mutex_lock(&mutex);
+        printf("before thread %d signaling money added\n", thread_id);
 
-            amount = rand() % MAX_PAYMENT;
-            if(rand()%2)
-                add_money(amount, thread_number);
-            else
+        pthread_cond_signal(&cond);
+
+        printf("after thread %d signaling money added\n", thread_id);
+    }
+
+    public: static void take_money_if_possible(int amount, int tn)
+    {
+            printf("money takers: %d\n", money_takers);
+            // if all other threads are waiting for more money - just add money or there'll be deadlock
+            if(money_takers == N-1)
             {
-                printf("money takers: %d\n", money_takers);
-                // if all other threads are waiting for more money - just add money or there'll be deadlock
-                if(money_takers == N-1)
-                {
-                    printf("thread %d wanted to take money but all other threads were waiting on adding money, so add_money will be called\n", thread_number);
-                    add_money(amount, thread_number);
-                    continue;
-                }
+                printf("thread %d wanted to take money but all other threads were waiting on adding money, so add_money will be called\n", tn);
+                Bank::add_money(amount, tn);
+            } else {
                 money_takers++;
-                take_money(amount + 20, thread_number);
+                take_money(amount + 20, tn);
                 money_takers--;
             }
-
-        }
     }
 
     public: static void init()
@@ -115,6 +98,29 @@ int Bank::money_takers;
 pthread_mutex_t Bank::mutex;
 pthread_cond_t Bank::cond;
 
+void* bank_user(void *args)
+{
+    int i = MAX_PER_USER_ITERATIONS;
+    int amount;
+    int thread_number = *((int *)args);
+
+    printf("here's thread %d\n", thread_number);
+    while(i--)
+    {
+        //pthread_mutex_lock(&mutex);
+
+        amount = rand() % MAX_PAYMENT;
+        if(rand()%2)
+        {
+            Bank::add_money(amount, thread_number);
+        }
+        else
+        {
+            Bank::take_money_if_possible(amount, thread_number);
+        }
+    }
+    pthread_exit(NULL);
+}
 
 int main()
 {
@@ -126,7 +132,7 @@ int main()
     {
         t_num = (int *)malloc(sizeof(int));
         *t_num = i;
-        pthread_create( &threads[i] , NULL , Bank::bank_user, t_num);
+        pthread_create( &threads[i] , NULL , bank_user, t_num);
     }
 
     for(i=0;i<N;i++)
